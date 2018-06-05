fa6713fd5d54b379f56d4d592f11708f6133c3eb
Ryan Swartzendruber <rswartzendruber13@gmail.com>
2018-06-05 16:06:37 -0400

    Finish Lesson 2 - SQL Joins

--- SQL for Data Analysis.sql
+++ SQL for Data Analysis.sql
@@ -169,5 +169,86 @@ JOIN accounts a
 	ON s.id = a.sales_rep_id
 JOIN orders o
 	ON o.account_id = a.id;
+
+----Last Check
+SELECT r.name region_name, s.name rep_name, a.name account_name
+FROM region r
+JOIN sales_reps s
+	ON r.id = s.region_id
+JOIN accounts a
+	ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest'
+ORDER BY a.name;
+
+SELECT r.name region_name, s.name rep_name, a.name account_name
+FROM region r
+JOIN sales_reps s
+	ON r.id = s.region_id
+JOIN accounts a
+	ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest'
+	AND s.name LIKE 'S%'
+ORDER BY a.name;
+
+SELECT r.name region_name, s.name rep_name, a.name account_name
+FROM region r
+JOIN sales_reps s
+	ON r.id = s.region_id
+JOIN accounts a
+	ON a.sales_rep_id = s.id
+WHERE r.name = 'Midwest'
+	AND s.name LIKE '% K%'
+ORDER BY a.name;
+
+SELECT r.name region_name, a.name account_name, o.total_amt_usd/(o.total + 0.01) AS unit_price
+FROM region r
+JOIN sales_reps s
+	ON r.id = s.region_id
+JOIN accounts a
+	ON s.id = a.sales_rep_id
+JOIN orders o
+	ON o.account_id = a.id
+WHERE o.standard_qty > 100;
+
+SELECT r.name region_name, a.name account_name, o.total_amt_usd/(o.total + 0.01) AS unit_price
+FROM region r
+JOIN sales_reps s
+	ON r.id = s.region_id
+JOIN accounts a
+	ON s.id = a.sales_rep_id
+JOIN orders o
+	ON o.account_id = a.id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price;
+
+SELECT r.name region_name, a.name account_name, o.total_amt_usd/(o.total + 0.01) AS unit_price
+FROM region r
+JOIN sales_reps s
+	ON r.id = s.region_id
+JOIN accounts a
+	ON s.id = a.sales_rep_id
+JOIN orders o
+	ON o.account_id = a.id
+WHERE o.standard_qty > 100 AND o.poster_qty > 50
+ORDER BY unit_price DESC;
+
+SELECT DISTINCT a.name account_name, w.channel
+FROM accounts a
+JOIN web_events w
+	ON a.id = w.account_id
+WHERE a.id = '1001';
+
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+FROM orders o
+JOIN accounts a
+	ON o.account_id = a.id
+WHERE o.occurred_at BETWEEN '01/01/2015' AND '12/31/2015'
+ORDER BY o.occurred_at DESC;
+
+
+
+
+
+
    
 
